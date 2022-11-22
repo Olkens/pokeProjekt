8b938fe2d848ee09345ab3c3a088895a4142f2ea
Michał Olkusz <michal.olkusz@logintegra.com>
2022-11-22 20:37:39 +0100

    Added routing and 2 basic routes

---
+++ src/components/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <router-link to="/pokemonPages/PokemonSearch">Wyszukiwarka</router-link>
+    <h1>Welcome home</h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage"
+}
+</script>
+
+<style scoped>
+
+</style>
