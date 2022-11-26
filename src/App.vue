89e84ddde460c70db22b5376f57e493e165f1890
olkens <olkusz.michal@gmail.com>
2022-11-26 21:33:42 +0100

    posprzątanie po nieudolnej próbie połączenia axiosa i vuex

--- src/App.vue
+++ src/App.vue
@@ -9,34 +9,12 @@
 </template>
 
 <script>
-import axios from 'axios';
-
-
 export default {
   name: 'App',
   components: {},
   data() {
     return {
-      url: 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=151',
-      poksy: [],
-      poksyDetails: [],
     }
-  },
-  mounted() {
-    axios.get(this.url).then((response) => {
-      const dataArr = response.data.results;
-      for (let i = 0; i < dataArr.length; i++) {
-        // console.log(dataArr[i].url);
-        this.poksy.push(dataArr[i])
-        axios.get(dataArr[i].url).then((res) => {
-          this.poksyDetails.push(res.data)
-        })
-      }
-    }).catch((err) => {
-      console.log(err)
-    }).finally(() => {
-
-    })
   },
 }
 </script>
