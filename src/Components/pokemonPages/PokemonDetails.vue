414fcbaa1332ea4432689456b874cc7bca8be43b
Lejonek <theleionpl@gmail.com>
2022-12-01 10:57:24 +0100

    Update PokemonDetails.vue

--- src/Components/pokemonPages/PokemonDetails.vue
+++ src/Components/pokemonPages/PokemonDetails.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div>
+    <div class="main">
       <router-link to="/"> Home </router-link>
     </div>
     {{pokemonDetails.name}}
@@ -26,5 +26,5 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 </style>
